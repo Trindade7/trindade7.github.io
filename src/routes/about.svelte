88bf82a529b45d75b77cfb366e7c19fc77bb45d0
Trindade7 <trindade.jose77@gmail.com>
2021-02-13 16:21:39 +0100

    style update

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,5 +5,7 @@
 <div class="container mx-auto px-4 flex flex-col items-center pt-40">
 	<h1 class="text-7xl font-serif font-bold text-center">COMING SOON</h1>
 
-	<h3 class="text-3xl pt-8 text-corporateDark-primary">Please be patient.</h3>
+	<h3 class="text-3xl pt-8 text-corporateDark-primary uppercase">
+		Please be patient.
+	</h3>
 </div>
